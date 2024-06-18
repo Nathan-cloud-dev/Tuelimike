38caa77a96ebf2852aac3955e564f96d2be1155a
Nathan-cloud-dev <kiprotichnathan027@gmail.com>
2024-06-18 13:35:49 +0300

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { createContext, useState } from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
@@ -6,18 +7,40 @@ import SignUp from './Components/SignUp';
 import Login from './Components/Login';
 import Courses from './Components/Courses';
 import Cart from './Components/Cart';
+import ProtectedRoute from './Components/ProtectedRoute';
+export const AppContext = createContext();
 
 function App() {
+  const accessToken = localStorage.getItem('access');
+  const isAuthenticated = accessToken ? true : false;
+  const [signedIn, setSignedIn] = useState(false);
+
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/courses" element={<Courses />} />
-      </Routes>
-      <Cart></Cart>
+      <AppContext.Provider value={{isAuthenticated, signedIn, setSignedIn}}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route 
+            path="/courses" 
+            element={
+            <ProtectedRoute>
+              <Courses />
+            </ProtectedRoute>
+            } 
+          />
+          <Route
+            path='/cart'
+            element={
+              <ProtectedRoute>
+                <Cart />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AppContext.Provider>
     </>
   );
 }
--- client/src/api.js
+++ client/src/api.js
@@ -1,3 +1,22 @@
-const api = 'http://127.0.0.1:8000/api';
+import axios from 'axios';
+
+const apiUrl = "http://127.0.0.1:8000/";
+
+const api = axios.create({
+    baseURL: apiUrl,
+})
+
+api.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('access');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
 
 export default api;
